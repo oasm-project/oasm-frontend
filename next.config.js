1c6509baf00037fa0e7bf21875397a89e43f0a18
herdeybayor <shereefdeen4real@gmail.com>
2023-03-29 03:28:11 +0100

    add env

--- next.config.js
+++ next.config.js
@@ -2,7 +2,8 @@
 const nextConfig = {
     reactStrictMode: true,
     env: {
-        BACKEND_BASE_URL: process.env.BACKEND_BASE_URL || "http://localhost:4000"
+        BACKEND_BASE_URL: process.env.BACKEND_BASE_URL || "http://localhost:4000",
+        BASE_URL: process.env.BASE_URL || "http://localhost:3000"
     }
 };
 
