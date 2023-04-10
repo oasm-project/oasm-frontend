51bc06e1f696801882a7de5ba2fe07746ca4aab1
herdeybayor <shereefdeen4real@gmail.com>
2023-04-10 20:34:36 +0100

    feat: add nprogress 🎉

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,13 @@ import type { AppProps } from "next/app";
 import Head from "next/head";
 import React from "react";
 import { Toaster } from "react-hot-toast";
+import Router from "next/router";
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
+
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
